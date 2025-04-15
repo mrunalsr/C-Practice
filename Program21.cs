3401a4c53aadedc0945a2cf2497f4ecfed98bd30
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 22:33:15 +0530

    Add files via upload

---
+++ Program21.cs
@@ -0,0 +1,18 @@
+﻿//C# Method Parameters
+using System;
+namespace std
+{
+    class Program{
+        static void MyMethod(string fname)
+        {
+            Console.WriteLine(fname + " Employee");
+        }
+        static void Main(string[] args)
+        {
+            MyMethod("Mrunal");
+            MyMethod("Sakshi");
+            MyMethod("Srushti");
+
+        }
+    }
+}
