075f6d3e900981b06455ec2e3eac60df6242ea41
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 23:28:39 +0530

    Add files via upload

---
+++ Program27.cs
@@ -0,0 +1,38 @@
+﻿//C# Method Overriding
+using System;
+namespace std{
+    class Animal{
+        public virtual void Sound()
+        {
+            Console.WriteLine("Animal makes a sound");
+        }
+    }
+    class Dog : Animal{
+        public override void Sound()
+        {
+            
+            Console.WriteLine("Dog Barks");
+        }
+    }
+    class Cat : Animal{
+        public override void Sound()
+        {
+            Console.WriteLine("Cat Meow");
+        }
+    }
+    class Program{
+        static void Main(string[] args)
+        {
+            Animal a = new Animal();
+            a.Sound();
+
+            Animal dog = new Dog();
+            dog.Sound();
+
+            Animal cat = new Cat();
+            cat.Sound();
+        }
+        
+
+    }
+}
