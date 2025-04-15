97894a60359bfdbb0d5670769227e8e5a8dcc0ae
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 22:47:49 +0530

    Add files via upload

---
+++ Program23.cs
@@ -0,0 +1,17 @@
+﻿//C# Default Parameters
+using System;
+namespace std{
+    class Program{
+        static void MyMethod(string country = "India")
+        {
+            Console.WriteLine(country);
+        }
+        static void Main(string[] args)
+        {
+            MyMethod("Norway");
+            MyMethod("Japan");
+            MyMethod();
+            MyMethod("USA");
+        }
+    }
+}
