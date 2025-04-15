7e8f5d14ae6f51803b0457fec46cae94ae10e502
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 10:10:11 +0530

    Add files via upload

---
+++ Program8.cs
@@ -0,0 +1,15 @@
+﻿//Logical Operators
+using System;
+namespace Hello{
+  class Program{
+    static void Main(string[] args)
+    {
+      int x = 6;
+      int y = 9;
+
+      Console.WriteLine("logical && " +(x>5 && y<10));
+      Console.WriteLine("logical || " +(x<5 || y<10));
+      Console.WriteLine("logical ! " +(!(x>5 && y<10)));
+    }
+  }
+}
