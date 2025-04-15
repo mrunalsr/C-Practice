603d5f1ce9ae1f4a7cdd4125b28b6222e2d83404
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 22:44:28 +0530

    Add files via upload

---
+++ Program22.cs
@@ -0,0 +1,16 @@
+﻿//C# Multiple Paramateres
+using System;
+namespace std{
+    class Program{
+        static void MyMethod(string fname, int age)
+        {
+            Console.WriteLine(fname +" is "+ age);
+        }
+        static void Main(string[] args)
+        {
+            MyMethod("Mrunal",22);
+            MyMethod("Sakshi",24);
+            MyMethod("Srushti",30);
+        }
+    }
+}
