2b11126f94e0690ad12a340e5f41c1a320d0c865
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 09:45:57 +0530

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,28 @@
+﻿//Typecasting in C# using inbuilt funtcions
+using System;
+namespace Helllo
+{
+  class Program4
+  {
+    static void Main(string[] args)
+    {
+      int num = 72;
+      double dnum = 7.2955;
+      char cnum = 'M';
+      bool bnum = true;
+
+      Console.WriteLine("Before Typecasting");
+
+      Console.WriteLine(num);
+      Console.WriteLine(dnum);
+      Console.WriteLine(cnum);
+      Console.WriteLine(bnum);
+
+      Console.WriteLine("After Typecasting");
+      Console.WriteLine(Convert.ToChar(num));
+      Console.WriteLine(Convert.ToInt32(dnum));
+      Console.WriteLine(Convert.ToInt32(cnum));
+      Console.WriteLine(Convert.ToInt32(bnum));
+    }
+  }
+}
