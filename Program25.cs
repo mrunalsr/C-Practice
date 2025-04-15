8b4cca8a18b18d37956aaf944b441d67be0bb5e9
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 22:58:38 +0530

    Add files via upload

---
+++ Program25.cs
@@ -0,0 +1,16 @@
+﻿//C# Named Arguments
+using System;
+namespace std{
+    class Program{
+        static void MyMethod(string child1, string child2, string child3)
+        {
+            Console.WriteLine("The youngest child is : " +child3);
+            Console.WriteLine("The second child is : " +child2);
+            Console.WriteLine("The first child is : " +child1);
+        }
+        static void Main(string[] args)
+        {
+            MyMethod(child3: "Mrunal", child1: "Sakshi", child2:"Srushti");
+        }
+    }
+}
