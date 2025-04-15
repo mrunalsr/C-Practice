c7933d578554588b6da6845a44c322272b75fa72
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 11:00:30 +0530

    Add files via upload

---
+++ Program14.cs
@@ -0,0 +1,13 @@
+﻿//for loop
+using System;
+namespace std{
+    class Program{
+        static void Main(string[] args)
+        {
+            for(int i =0;i<6;i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
