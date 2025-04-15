1b15b3242b9cb7147d1af8521796f6c3ac152743
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 10:58:09 +0530

    Add files via upload

---
+++ Program13.cs
@@ -0,0 +1,32 @@
+﻿//while and do-while loop
+
+/*using System;
+namespace std
+{
+  class Program{
+    static void Main(string[] args)
+    {
+      int i =6;
+      while( i != 0)
+      {
+        Console.WriteLine(i);
+        i--;
+      }
+    }
+  }
+}*/
+
+using System;
+namespace std{
+  class Program{
+    static void Main(string[] args)
+    {
+      int i = 0;
+      do{
+        Console.WriteLine(i);
+        i++;
+      }
+      while(i<=5);
+    }
+  }
+}
