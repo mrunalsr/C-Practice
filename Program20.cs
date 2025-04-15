0559621aa574de87bdb90a4ead2cfe8ee030dd74
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 22:29:04 +0530

    Add files via upload

---
+++ Program20.cs
@@ -0,0 +1,15 @@
+﻿//C# Methods
+//Static method in C# 
+using System;
+namespace std{
+    class Program{
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed");
+        }
+        static void Main(string[] args)
+        {
+            MyMethod();
+        }
+    }
+}
