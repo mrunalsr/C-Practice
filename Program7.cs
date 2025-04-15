d7c075e32a6a0c212828f748fbf5d896d69cd192
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 10:06:00 +0530

    Add files via upload

---
+++ Program7.cs
@@ -0,0 +1,19 @@
+﻿//3) Comparison operator
+using System;
+namespace Hello{
+  class Program{
+    static void Main(string[] args)
+    {
+      int x = 10;
+      int y = 12;
+      
+      Console.WriteLine("Operator == " +(x==y));
+      Console.WriteLine("Operator != " +(x!=y));
+      Console.WriteLine("Operator == " +(x>y));
+      Console.WriteLine("Operator == " +(x<y));
+      Console.WriteLine("Operator == " +(x>=y));
+      Console.WriteLine("Operator == " +(x<=y));
+      
+    }
+  }
+}
