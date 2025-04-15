165a837172b68d99e4707b5df2164ef6062fec38
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 10:32:11 +0530

    Add files via upload

---
+++ Program10.cs
@@ -0,0 +1,31 @@
+﻿//C# Strings methods
+using System;
+namespace Hello
+{
+  class Program{
+    static void Main(string[] args)
+    {
+      string str = "Mrunal Raksale";
+      string str1 = "Mansi Raksale";
+
+      //Get Length of String
+      Console.WriteLine("Length " + str.Length);
+      //Convert to upper case
+      Console.WriteLine("Toupper " + str.ToUpper());
+      //Convert to lowercase
+      Console.WriteLine("Tolower " + str.ToLower());
+      //Concat string
+      Console.WriteLine("Concate " + String.Concat(str, str1));
+      Console.WriteLine("Concate " + (str+str1));
+
+      //string interpolation
+      Console.WriteLine($"My name is {str} and My sister name is {str1}");
+
+      //Access strings
+      Console.WriteLine(str[4]);
+      Console.WriteLine(str.IndexOf('R'));
+      Console.WriteLine(str.Substring(7));
+
+    }
+  }
+}
