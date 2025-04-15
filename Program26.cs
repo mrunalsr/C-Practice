bb0dfcc063fae26de5f6e8bf6601c95f6c312202
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 23:04:51 +0530

    Add files via upload

---
+++ Program26.cs
@@ -0,0 +1,23 @@
+﻿//C# Method Overloading
+using System;
+namespace std
+{
+    class Program{
+        static int MyMethod(int x)
+        {
+            return x+7;
+        }
+        static float MyMethod(float x)
+        {
+            return x + 7;
+        }
+        static void Main(string[] args)
+        {
+            int intResult = MyMethod(5);
+            float floatResult = MyMethod(5.2656f);
+
+            Console.WriteLine("Result for int : " +intResult);
+            Console.WriteLine("Result for float : " +floatResult);
+        }
+    }
+}
