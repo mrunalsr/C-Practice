5e6549a25300ec1a90e73d6e852c00c679b02620
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 09:32:54 +0530

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,15 @@
+﻿//Implicit Typecasting
+using System;
+namespace Hello
+{
+  class Program2
+  {
+    static void Main(string[] args)
+    {
+      int num = 9;
+      double mynum = num;
+      Console.WriteLine(num);
+      Console.WriteLine(mynum);
+    }
+  }
+}
