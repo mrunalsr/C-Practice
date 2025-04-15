206af110307c5d0d3fce0c67da9fdf094cf22d52
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 22:21:05 +0530

    Add files via upload

---
+++ Program19.cs
@@ -0,0 +1,18 @@
+﻿//C# Multidimensional array
+using System;
+namespace std{
+    class Program{
+        static void Main(string[] args)
+        {
+            int[,] num = {{1,4,2},{3,6,8}};
+            //using for loop
+            for(int i=0;i<num.GetLength(0);i++)
+            {
+                for(int j=0;j<num.GetLength(1);j++)
+                {
+                    Console.WriteLine(num[i,j]);
+                }
+            }
+        }
+    }
+}
