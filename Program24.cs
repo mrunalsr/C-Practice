97cee96bbcc169c5bf1d3fd40e57965f10a4c505
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 22:52:27 +0530

    Add files via upload

---
+++ Program24.cs
@@ -0,0 +1,14 @@
+﻿//C# Return values from methods
+using System;
+namespace std{
+    class Program{
+        static int MyMethod(int x)
+        {
+            return x+5;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyMethod(3));
+        }
+    }
+}
