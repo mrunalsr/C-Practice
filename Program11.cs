8353cba3690078b32e7ef008d7263013a8c14e13
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 10:37:44 +0530

    Add files via upload

---
+++ Program11.cs
@@ -0,0 +1,26 @@
+﻿//C# If-else
+using System;
+namespace HEllo
+{
+  class Program{
+    static void Main(string[] args)
+    {
+      int x = 5;
+      int y = 9;
+      /*if(x<y)
+      {
+        Console.WriteLine("y is greater");
+      }
+      else if(x>y)
+      {
+        Console.WriteLine("X is greater");
+      }
+      else{
+        Console.WriteLine("Error");
+      }*/
+      //short-hand if-else
+      string result = (x>y) ? "x is greater" : "y is greater";
+      Console.WriteLine(result);
+    }
+  }
+}
