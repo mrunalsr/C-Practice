2c2729625cef53cc0c4bef41598d1745623f16e8
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 10:16:44 +0530

    Add files via upload

---
+++ Program9.cs
@@ -0,0 +1,17 @@
+﻿//C# Math functions
+using System;
+namespace Hello
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Math.Max(5,10));
+      Console.WriteLine(Math.Min(5,10));
+      Console.WriteLine(Math.Sqrt(16));
+      Console.WriteLine(Math.Abs(-4));
+      Console.WriteLine(Math.Round(5.2659956));
+      
+    }
+  }
+}
