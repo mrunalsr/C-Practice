ea99d4a82c89596f4ff1cd66bbff80fa36e896b9
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 09:54:13 +0530

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,29 @@
+﻿//Operators in C#
+//1)Arithemtetic Operators
+using System;
+namespace Hello
+{
+  class Program{
+    static void Main(string[] args)
+    {
+      //1)Arithemetic operators
+      int x = 9;
+      int y = 5;
+      Console.WriteLine("Addition : " +(x+y));
+      Console.WriteLine("Substraction : " +(x-y));
+      Console.WriteLine("Multiplication : " +(x*y));
+      Console.WriteLine("Division : " +(x/y));
+      Console.WriteLine("Modulus : " +(x%y));
+      Console.WriteLine("Increment : " +(x++));
+      Console.WriteLine("After Increment : " +(x));
+      Console.WriteLine("Decrement : " +(y--));
+      Console.WriteLine("After decrement : " +(y));
+      Console.WriteLine("pre-increment : " +(++x));
+      Console.WriteLine("pre-decrement : " +(--y));
+
+
+
+      
+    }
+  }
+} 
