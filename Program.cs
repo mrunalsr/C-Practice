f68e853c8cf1599afbc7d0a8003551d59680aa5e
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 09:16:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        int num = 32;
+        //Console.ReadLine(num); error
+        Console.WriteLine("The given number is : " +num);    
+    }
+  }
+}
---
+++ Program1.cs
@@ -0,0 +1,18 @@
+using System;
+namespace HelloWorld
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1,2,3,4,5};
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+        }
+    }
+}
