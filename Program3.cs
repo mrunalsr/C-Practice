8b773af6eff4facb39411e56a108471fcb4ce69f
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 09:35:23 +0530

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,16 @@
+﻿//Explicit Typecasting
+using System;
+namespace Hello
+{
+  class Program3
+  {
+    static void Main(string[] args)
+    {
+      double dnum = 9.25862;
+      int num = (int)dnum;
+      Console.WriteLine(dnum);
+      Console.WriteLine(num);
+
+    }
+  }
+}
