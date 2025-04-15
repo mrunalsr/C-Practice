9ed43ef32e2d1e8cac5a389db56d29dfe8d1643a
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 11:19:19 +0530

    Add files via upload

---
+++ Program16.cs
@@ -0,0 +1,24 @@
+﻿//C# Arrays
+using System;
+namespace std{
+    class PRogram{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of array : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] numbers = new int[size];
+
+            for(int i=0;i<size;i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nYou enterred the following numbers:");
+            for(int i=0;i< size;i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
