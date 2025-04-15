e03152c278aefaa63bcdc416d79268aa14d427da
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 10:01:21 +0530

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Hello
+{
+  class Program{
+    static void Main(string[] args)
+    {
+      //2)Assignment operator
+      int x = 5;
+      Console.WriteLine("Operator = " +(x));
+      Console.WriteLine("Operator += " +(x+=3));
+      Console.WriteLine("Operator -= " +(x-=3));
+      Console.WriteLine("Operator *= " +(x*=3));
+      Console.WriteLine("Operator /= " +(x/=3));
+      Console.WriteLine("Operator %= " +(x%=3));
+      Console.WriteLine("Operator &= " +(x&=3));
+      Console.WriteLine("Operator |= " +(x|=3));
+      Console.WriteLine("Operator ^= " +(x^=3));
+      Console.WriteLine("Operator >>= " +(x>>3));
+      Console.WriteLine("Operator = " +(x));
+      Console.WriteLine("Operator <<= " +(x<<3));
+      Console.WriteLine("Operator = " +(x));
+
+    }
+  }
+}
