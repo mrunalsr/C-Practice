9b8f03be3ce825aff2a86da1897f31b9f39ee7f6
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 22:18:19 +0530

    Add files via upload

---
+++ Program18.cs
@@ -0,0 +1,18 @@
+﻿//C# Multidimensional array
+using System;
+namespace std{
+    class Program{
+        static void Main(string[] args)
+        {
+            int[,] num = {{1,4,2},{3,6,8}};
+            Console.WriteLine(num[0,2]);
+            Console.WriteLine(num[0,0]);
+
+            //using foreach loop
+            foreach(int i in num)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
