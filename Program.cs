924302bb88367c46f54d106b7cb2a844538c9488
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-15 09:28:25 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
-﻿//Use of const keyword
+﻿//Data Types in C#
 using System;
 namespace HelloWorld{
   class Program{
     static void Main(string[] args)
     {
-      const int num = 10;
-      //num = 20 error
+      int num = 10;
+      char cnum = 'A';
+      double dnum = 62.52565;
+      float fnum = 52.36f;
+      long lnum = 85255558989888484;
+      bool bnum = true;
+      string snum = "Mrunal";
+
       Console.WriteLine(num);
+      Console.WriteLine(cnum);
+      Console.WriteLine(fnum);
+      Console.WriteLine(bnum);
+      Console.WriteLine(snum);
+      Console.WriteLine(dnum);
+      Console.WriteLine(lnum);
+
+      
+
     }
   }
 }
