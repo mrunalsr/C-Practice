e0e31f96850f3db85971f84a2b8d6905e011a8bf
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-16 09:28:48 +0530

    Add files via upload

---
+++ Program29.cs
@@ -0,0 +1,19 @@
+﻿//C# Multiple objects of one class
+using System;
+namespace std{
+    class Car{
+        string name = "Mrunal";
+        string lname = "Raksale";
+        string mname = "jyoti";
+        static void Main(string[] args){
+            Car obj1 = new Car();
+            Car obj2 = new Car();
+
+            Console.WriteLine(obj1.name);
+            Console.WriteLine(obj2.lname);
+            Console.WriteLine(obj1.mname);
+
+
+        }
+    }
+}
