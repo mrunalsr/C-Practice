28d9efb7b1d4e4b4f2ed3748dd4c636ef669e6e6
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-16 09:36:55 +0530

    Add files via upload

---
+++ Program30.cs
@@ -0,0 +1,20 @@
+﻿//C# Class Memebers
+using System;
+using System.Security.Cryptography;
+namespace std{
+    class Car{
+        string name = "Mrunal";
+        int age = 21;
+        public void full()
+        {
+            Console.WriteLine($"Hello from {name}");
+        }
+        static void Main(string[] args)
+        {
+            Car obj = new Car();
+            Console.WriteLine(obj.name);
+            Console.WriteLine(obj.age);
+            obj.full();
+        }
+    }
+}
