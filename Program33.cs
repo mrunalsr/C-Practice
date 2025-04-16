9931d13d34a8780da1fb732937b078d500109058
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2025-04-16 10:47:31 +0530

    Add files via upload

--- Program33.cs
+++ Program33.cs
@@ -1,37 +1,34 @@
-﻿//C# Inheritance
+﻿//C# Abstraction
 using System;
 namespace std{
-    /*sealed*/class Health{
-        public string name = "Mrunal";
-        public void Drink()
+    public abstract class Animal{
+        string name = "Dog";
+        public abstract void Sound();
+        public void Sleep()
         {
-            Console.WriteLine("Drink 5Li of water everyday");
+            Console.WriteLine("zzz");
         }
     }
-    class Person : Health{
-        public string level = "3lit";
-
-    }
-    //sealed class
-    sealed class BP : Health
-    {
-        public string bp = "345";
-        public void Test(){
-            Console.WriteLine("Due to drininking water daily my BP is in contrl now");
-
+    class Pig : Animal{
+        public override void Sound()
+        {
+            Console.WriteLine("Wee Wee");
         }
-        
+    }
+    class Dog : Animal{
+        public override void Sound()
+        {
+            throw new NotImplementedException();
+            Console.WriteLine("Bow BOw");
+        }
     }
     class Program{
         static void Main(string[] args)
         {
-            Person obj = new Person();
-            obj.Drink();
-            Console.WriteLine($"Hii My name is {obj.name} and everyday I drink {obj.level} of water");
-            BP obj1 = new BP();
-            Console.WriteLine($"My BP is {obj1.bp}");
-            obj1.Drink();
-            
+            Pig obj = new Pig();
+            Dog obj1 = new Dog();
+            obj.Sound();
+            obj.Sleep();
         }
     }
 }
